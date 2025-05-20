284c05b388e187db2e3fc0ab0c12bf78356a67f3
Bhavesh-Mankar51 <bhaveshmankar770@gmail.com>
2025-05-20 10:32:59 +0530

    Add billing info form

--- payment/views.py
+++ payment/views.py
@@ -1,5 +1,45 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from cart.cart import Cart
+from payment.forms import ShippingForm, PaymentForm
+from payment.models import ShippingAddress
+from django.contrib import messages
 # Create your views here.
+
+def billing_info(request):
+    if request.POST:
+		# Get the cart
+        cart = Cart(request)
+        cart_products = cart.get_prods
+        quantities = cart.get_quants
+        totals = cart.cart_total()
+        if request.user.is_authenticated:
+            billing_form = PaymentForm()
+            return render(request, "payment/billing_info.html", {"cart_products":cart_products, "quantities":quantities, "totals":totals, "shipping_info":request.POST, "billing_form" : billing_form})
+        else:
+            billing_info = PaymentForm()
+            return render(request, "payment/billing_info.html", {"cart_products":cart_products, "quantities":quantities, "totals":totals, "shipping_info":request.POST, "billing_form" : billing_form})
+        shipping_form = request.POST
+        return render(request, "payment/billing_info.html", {"cart_products":cart_products, "quantities":quantities, "totals":totals, "shipping_form":shipping_form})
+    else:
+        messages.success(request, "Access Denied")
+        return redirect('home')
+
+
+def checkout(request):
+	# Get the cart
+	cart = Cart(request)
+	cart_products = cart.get_prods
+	quantities = cart.get_quants
+	totals = cart.cart_total()
+
+	if request.user.is_authenticated:
+		# Checkout as logged in user
+		# Shipping User
+		shipping_user = ShippingAddress.objects.get(user__id=request.user.id)
+		# Shipping Form
+		shipping_form = ShippingForm(request.POST or None, instance=shipping_user)
+		return render(request, "payment/checkout.html", {"cart_products":cart_products, "quantities":quantities, "totals":totals, "shipping_form":shipping_form })
+
+
 def payment_success(request):
     return render(request, 'payment/payment_success.html', {})
